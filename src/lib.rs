8fe3451cc0f263684ad22563e30f85043dfeb284
steven <steven@nztech.ca>
2024-05-30 02:08:52 -0700

    access rust board in python

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,50 @@
 use pyo3::prelude::*;
 
+#[pyclass(module = "ChessProject", get_all, set_all)]
+struct GameState {
+    board_size: usize,
+}
+
+#[pymethods]
+impl GameState {
+    #[new]
+    fn new(board_size: usize) -> Self {
+        GameState{board_size}
+    }
+
+    fn __str__(&self) -> PyResult<String> {
+        Ok(format!("Board Size: {}", self.board_size))
+    }
+}
+
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+/// Macro to add classes to PyModule
+macro_rules! add_classes {
+    ($module:ident, $($class:ty),+) => {
+        $(
+            $module.add_class::<$class>()?;
+        )+
+    };
+}
+
+/// Macro to add functions to PyModule
+macro_rules! add_functions {
+    ($module:ident, $($function:ident),+) => {
+        $(
+            $module.add_wrapped(wrap_pyfunction!($function))?;
+        )+
+    };
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn ChessProject(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    add_functions!(m, sum_as_string);
+    add_classes!(m, GameState);
     Ok(())
 }
--- src/test_pyrust.py
+++ src/test_pyrust.py
@@ -2,3 +2,6 @@ import ChessProject # rust library
 
 result = ChessProject.sum_as_string(1, 2)
 print(result)
+
+a = ChessProject.GameState(8)
+print(a)
