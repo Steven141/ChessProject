b80e4e128d7bababbfbf1d56ec874986357b7ea0
steven <steven@nztech.ca>
2024-05-30 17:31:39 -0700

    add base rust engine classes

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
 use pyo3::prelude::*;
 
+
+/// EXAMPLE: Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+
+/// Keeps track of game state, possible moves, and old moves
 #[pyclass(module = "ChessProject", get_all, set_all)]
 struct GameState {
     board_size: usize,
 }
+
 
 #[pymethods]
 impl GameState {
@@ -17,11 +27,57 @@ impl GameState {
     }
 }
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+
+/// Keeps track of whether the kings have the right to castle
+#[pyclass(module = "ChessProject", get_all, set_all)]
+struct CastleRights {
+    wks: bool,
+    bks: bool,
+    wqs: bool,
+    bqs: bool,
 }
+
+
+#[pymethods]
+impl CastleRights {
+    #[new]
+    fn new(wks: bool, bks: bool, wqs: bool, bqs: bool) -> Self {
+        CastleRights{wks, bks, wqs, bqs}
+    }
+
+    fn __str__(&self) -> PyResult<String> {
+        Ok(format!("wks={} bks={} wqs={} bqs={}", self.wks, self.bks, self.wqs, self.bqs))
+    }
+}
+
+
+/// Holds information about a specific move
+#[pyclass(module = "ChessProject", get_all, set_all)]
+struct Move {
+    start_sq: (i32, i32),
+    end_sq: (i32, i32),
+    is_enpassant_move: bool,
+    is_castle_move: bool,
+}
+
+
+#[pymethods]
+impl Move {
+    #[new]
+    fn new(start_sq: (i32, i32), end_sq: (i32, i32), is_enpassant_move: Option<bool>, is_castle_move: Option<bool>) -> Self {
+        Move {
+            start_sq,
+            end_sq,
+            is_enpassant_move: is_enpassant_move.unwrap_or(false),
+            is_castle_move: is_castle_move.unwrap_or(false),
+        }
+    }
+
+    fn __str__(&self) -> PyResult<String> {
+        Ok(format!("Start={:?} End={:?} Enpassant={} Castle={}", self.start_sq, self.end_sq, self.is_enpassant_move, self.is_castle_move))
+    }
+}
+
 
 /// Macro to add classes to PyModule
 macro_rules! add_classes {
@@ -32,6 +88,7 @@ macro_rules! add_classes {
     };
 }
 
+
 /// Macro to add functions to PyModule
 macro_rules! add_functions {
     ($module:ident, $($function:ident),+) => {
@@ -41,10 +98,11 @@ macro_rules! add_functions {
     };
 }
 
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn ChessProject(_py: Python, m: &PyModule) -> PyResult<()> {
     add_functions!(m, sum_as_string);
-    add_classes!(m, GameState);
+    add_classes!(m, GameState, CastleRights, Move);
     Ok(())
 }
--- src/test_pyrust.py
+++ src/test_pyrust.py
@@ -5,3 +5,9 @@ print(result)
 
 a = ChessProject.GameState(8)
 print(a)
+
+c = ChessProject.CastleRights(True, True, True, False)
+print(c)
+
+m = ChessProject.Move((0,1), (2,3), is_castle_move=True)
+print(m)
