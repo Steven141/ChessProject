e0424a2e7b69ae2fb8efa046a92b1ba10b754ed7
steven <steven@nztech.ca>
2024-06-03 01:03:31 -0700

    generate hor and vert moves

--- src/engine_bitboard.py
+++ src/engine_bitboard.py
@@ -15,14 +15,14 @@ class GameState():
         #     ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR'],
         # ]
         self.board = [
+            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+            ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
             ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', 'bP', '--', '--'],
+            ['--', 'bP', '--', '--', 'wQ', '--', '--', '--'],
             ['--', '--', '--', '--', '--', '--', '--', '--'],
-            ['--', '--', '--', '--', '--', '--', '--', '--'],
-            ['--', 'wP', 'bP', '--', 'bP', 'wP', 'bP', '--'],
-            ['--', '--', '--', '--', '--', '--', '--', '--'],
-            ['--', '--', '--', '--', '--', '--', '--', '--'],
-            ['--', '--', '--', '--', '--', '--', '--', '--'],
-            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR'],
         ]
         # piece bitboards
         self.wP = self.wN = self.wB = self.wR = self.wQ = self.wK = 0
@@ -35,25 +35,18 @@ class GameState():
             binary = '0000000000000000000000000000000000000000000000000000000000000000'
             binary = binary[:i] + '1' + binary[i+1:]
             match self.board[i // 8][i % 8]:
-                case 'wP': self.wP += self.convertStringToBitboard(binary)
-                case 'wN': self.wN += self.convertStringToBitboard(binary)
-                case 'wB': self.wB += self.convertStringToBitboard(binary)
-                case 'wR': self.wR += self.convertStringToBitboard(binary)
-                case 'wQ': self.wQ += self.convertStringToBitboard(binary)
-                case 'wK': self.wK += self.convertStringToBitboard(binary)
-                case 'bP': self.bP += self.convertStringToBitboard(binary)
-                case 'bN': self.bN += self.convertStringToBitboard(binary)
-                case 'bB': self.bB += self.convertStringToBitboard(binary)
-                case 'bR': self.bR += self.convertStringToBitboard(binary)
-                case 'bQ': self.bQ += self.convertStringToBitboard(binary)
-                case 'bK': self.bK += self.convertStringToBitboard(binary)
-
-
-    def convertStringToBitboard(self, binary) -> int:
-        int_rep = int(binary, 2)
-        if binary[0] == '1': # negative, do 2's comp
-            int_rep -= (1 << len(binary))
-        return int_rep
+                case 'wP': self.wP += BinaryOps.convertStringToBitboard(binary)
+                case 'wN': self.wN += BinaryOps.convertStringToBitboard(binary)
+                case 'wB': self.wB += BinaryOps.convertStringToBitboard(binary)
+                case 'wR': self.wR += BinaryOps.convertStringToBitboard(binary)
+                case 'wQ': self.wQ += BinaryOps.convertStringToBitboard(binary)
+                case 'wK': self.wK += BinaryOps.convertStringToBitboard(binary)
+                case 'bP': self.bP += BinaryOps.convertStringToBitboard(binary)
+                case 'bN': self.bN += BinaryOps.convertStringToBitboard(binary)
+                case 'bB': self.bB += BinaryOps.convertStringToBitboard(binary)
+                case 'bR': self.bR += BinaryOps.convertStringToBitboard(binary)
+                case 'bQ': self.bQ += BinaryOps.convertStringToBitboard(binary)
+                case 'bK': self.bK += BinaryOps.convertStringToBitboard(binary)
     
 
     def drawGameArray(self) -> None:
@@ -88,16 +81,6 @@ class GameState():
             print(*r)
 
 
-    def drawArrayFromBitboard(self, bitboard) -> None:
-        new_board = [['0']*8 for _ in range(8)]
-        for i in range(64):
-            shift = 64 - 1 - i
-            if (bitboard >> shift) & 1 == 1:
-                new_board[i // 8][i % 8] = '1'
-        for r in new_board:
-            print(*r)
-
-
 class Moves():
     def __init__(self) -> None:
         # specific bitboards
@@ -113,22 +96,23 @@ class Moves():
         self.extended_centre = 66229406269440
         self.king_side = 1085102592571150095
         self.queen_side = -1085102592571150096
-        self.king_b7 = 0
-        self.knight_c6 = 0
+        self.king_b7 = -2260560722335367168 # where b7 king can attack
+        self.knight_c6 = 5802888705324613632 # where c6 knight can attack
         self.not_white_pieces = 0 # all pieces white can capture (not black king)
         self.black_pieces = 0 # black pieces but no black king
         self.empty = 0
+        self.occupied = 0
 
         self.rank_masks = [
-            self.rank_1,
-            65280,
-            16711680,
-            self.rank_4,
-            self.rank_5,
-            280375465082880,
-            71776119061217280,
             self.rank_8,
-        ] # from rank 1 to rank 8
+            71776119061217280,
+            280375465082880,
+            self.rank_5,
+            self.rank_4,
+            16711680,
+            65280,
+            self.rank_1,
+        ] # from rank 8 to rank 1
         self.file_masks = [
             self.file_a,
             4629771061636907072,
@@ -139,13 +123,50 @@ class Moves():
             144680345676153346,
             self.file_h,
         ] # from file a to file h
+        self.diagonal_masks = [
+            -9223372036854775808,
+            4647714815446351872,
+            2323998145211531264,
+            1161999622361579520,
+            580999813328273408,
+            290499906672525312,
+            145249953336295424,
+            72624976668147840,
+            283691315109952,
+            1108169199648,
+            4328785936,
+            16909320,
+            66052,
+            258,
+            1,
+        ] # from top left to bottom right
+        self.anti_diagonal_masks = [
+            72057594037927936,
+            144396663052566528,
+            288794425616760832,
+            577588855528488960,
+            1155177711073755136,
+            2310355422147575808,
+            4620710844295151872,
+            -9205322385119247871,
+            36099303471055874,
+            141012904183812,
+            550831656968,
+            2151686160,
+            8405024,
+            32832,
+            128,
+        ] # from top right to bottom left
 
 
     def possibleMovesW(self, history, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK) -> str:
         self.not_white_pieces = ~(wP|wN|wB|wR|wQ|wK|bK) # avoid illegal bK capture
         self.black_pieces = bP|bN|bB|bR|bQ # avoid illegal bK capture
         self.empty = ~(wP|wN|wB|wR|wQ|wK|bP|bN|bB|bR|bQ|bK)
+        self.occupied = ~self.empty
         move_list = self.possibleWP(history, wP, bP)
+        # x = self.possibleHAndVMoves(36)
+        # BinaryOps.drawArrayFromBitboard(x)
 
         return move_list
 
@@ -221,12 +242,76 @@ class Moves():
         return move_list
 
 
+    """
+    Returns all possible horizontal and vertical moves of piece at index piece_idx
+
+    Example for formula derivation:
+    occupied = o = 11000101 -> wP wP -- -- -- bR -- wP
+    slider = s = 00000100
+    o - s = 11000001 -> removes slider bit
+    o - 2s = 10111101 -> flips bits left of slider bit until first seen occupied bit (inclusive)
+    left = o^(o-2s) = 01111000 -> extracts all possible left sliding positions including first taken piece
+    let o' denote reverse of o
+    right = (o'^(o'-2s'))' = o^(o'-2s')' = 00000011
+    lineAttacks_h = right^left = o^(o'-2s')' ^ o^(o-2s) = (o'-2s')' ^ (o-2s)
+    m = mask
+    lineAttacks_v = (((o&m)'-2s')' ^ ((o&m)-2s))
+
+    return (possible_h & rank_m) | (possible_v & file_m) to only consider one file and rank
+    """
+    def possibleHAndVMoves(self, piece_idx) -> int:
+        # piece_idx = 0 -> top left of board -> 1000...000
+        binary_idx = 1 << (64 - 1 - piece_idx)
+        rank_mask = self.rank_masks[piece_idx // 8]
+        file_mask = self.file_masks[piece_idx % 8]
+
+        possible_h = (self.occupied - 2*binary_idx) ^ BinaryOps.reverseBits(BinaryOps.reverseBits(self.occupied) - 2*BinaryOps.reverseBits(binary_idx))
+        possible_v = ((self.occupied & file_mask) - 2*binary_idx) ^ BinaryOps.reverseBits(BinaryOps.reverseBits(self.occupied & file_mask) - 2*BinaryOps.reverseBits(binary_idx))
+
+        return (possible_h & rank_mask) | (possible_v & file_mask)
+
+
+class BinaryOps():
+    @staticmethod
+    def convertStringToBitboard(binary) -> int:
+        int_rep = int(binary, 2)
+        if binary[0] == '1': # negative, do 2's comp
+            int_rep -= (1 << len(binary))
+        return int_rep
+
+
+    @staticmethod
+    def drawArrayFromBitboard(bitboard) -> None:
+        new_board = [['0']*8 for _ in range(8)]
+        for i in range(64):
+            shift = 64 - 1 - i
+            if (bitboard >> shift) & 1 == 1:
+                new_board[i // 8][i % 8] = '1'
+        for r in new_board:
+            print(*r)
+
+
+    """
+    Takes an int as input and returns an int representing the reverse of the inputed binary
+
+    Special Cases:
+    1. append a postfix of 1 for negative numbers
+    2. bin() does not keep leading zeros to use f'{int64:064b}'
+        - int64 -> int to convert to binary string
+        - : -> everything after this is the format specifier
+        - 0 -> pad with zeros
+        - 64 -> pad to a total length off 64
+        - b -> use binary representation for the number
+    """
+    @staticmethod
+    def reverseBits(int64) -> int:
+        consider_window = int('1'*64, 2)
+        bin_str = f'{int64 & consider_window :064b}'[::-1]
+        return BinaryOps.convertStringToBitboard(bin_str)
+
+
 g = GameState()
-# g.drawGameArray()
-# g.drawArrayFromBitboard(g.wP)
-# print(g.wP)
+g.drawGameArray()
 
 m = Moves()
 move_list = m.possibleMovesW('', g.wP, g.wN, g.wB, g.wR, g.wQ, g.wK, g.bP, g.bN, g.bB, g.bR, g.bQ, g.bK)
-# g.drawArrayFromBitboard(moves)
-print(move_list)
